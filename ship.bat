b0b8ca6953043eb2344af7701e6ba707637da4ac
Alvin <kimbowa2alvin@gmail.com>
2026-01-20 19:38:14 -0800

    feat: update build scripts for UltAIViewer
    - change build script to use UltrAiViewer.spec
    - add Windows shipping script to package build output
    - create VERSION file to manage application versioning

---
+++ ship.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+set /p VERSION=<VERSION
+set "OS=windows"
+set "ARCH=x86"
+set "APP_NAME=UltAIViewer-v%VERSION%-%OS%-%ARCH%"
+set "ARCHIVE_NAME=%APP_NAME%.zip"
+
+if not exist "dist\%APP_NAME%" (
+  echo Missing build output: dist\%APP_NAME%
+  exit /b 1
+)
+
+tar -a -cvf "%ARCHIVE_NAME%" -C dist "%APP_NAME%"
+if errorlevel 1 exit /b 1
+
+echo Shipped: %ARCHIVE_NAME%
